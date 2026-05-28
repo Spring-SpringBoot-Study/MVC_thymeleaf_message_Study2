9f3c216a4b0b1dac692cfac762ca56f5de532f9d
ChaeHyunLim <chaehyun010104@gmail.com>
2026-05-28 19:33:21 +0900

    section 4 : 스프링 메시지 소스 설정 및 사용

--- src/test/java/hello/itemservice/ItemServiceApplicationTests.java
+++ src/test/java/hello/itemservice/ItemServiceApplicationTests.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootTest
 class ItemServiceApplicationTests {
@@ -9,5 +12,14 @@ class ItemServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new
+//				ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 
 }
